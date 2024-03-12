ee56fa3b2aab524abea2b3b12d1ee45d32cc8905
Daniel Wang <48870892+DanWang725@users.noreply.github.com>
2024-03-11 20:51:30 -0400

    #18 fix syntax error

--- src/index.js
+++ src/index.js
@@ -8,9 +8,7 @@ import NiceModal from '@ebay/nice-modal-react';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <NiceModal.Provider>
-      <App />
-    </NiceModal.Provider>
+    <App />
   </React.StrictMode>,
 );
 
