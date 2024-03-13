cc2f271302b09258512925b3b0758061ac149956
Daniel Wang <48870892+DanWang725@users.noreply.github.com>
2024-03-12 21:10:46 -0400

    #24 update provider, replace routes with app content

--- src/app/Providers.js
+++ src/app/Providers.js
@@ -2,13 +2,16 @@ import NiceModal from '@ebay/nice-modal-react';
 import { ThemeProvider } from '@mui/material';
 import { theme } from './themeUtils';
 import { Toaster } from 'react-hot-toast';
+import { BrowserRouter } from 'react-router-dom';
 
 const Providers = ({ children }) => {
   return (
     <>
       <NiceModal.Provider>
-        <Toaster position="bottom-center" reverseOrder />
-        <ThemeProvider theme={theme}>{children}</ThemeProvider>
+        <BrowserRouter>
+          <Toaster position="bottom-center" reverseOrder />
+          <ThemeProvider theme={theme}>{children}</ThemeProvider>
+        </BrowserRouter>
       </NiceModal.Provider>
     </>
   );
