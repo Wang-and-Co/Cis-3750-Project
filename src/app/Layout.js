c703eca84f5eca31eb17cdda894f5537518f6174
Ethan Wang <wange@uoguelph.ca>
2024-03-14 11:26:30 -0400

    added my events section

--- src/app/Layout.js
+++ src/app/Layout.js
@@ -8,6 +8,7 @@ import {
   ListItemButton,
   ListItemIcon,
   ListItemText,
+  Toolbar,
   Typography,
 } from '@mui/material';
 
@@ -16,8 +17,13 @@ const Layout = ({ children }) => {
     <>
       <CssBaseline />
       <AppBar sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
-        <Typography>Test</Typography> {/* TODO add a real app bar */}
+        <Toolbar>
+          <IcecreamOutlined />
+          <Typography variant='h4'>Test search bar</Typography> 
+          {/* TODO add a real app bar */}
+        </Toolbar>
       </AppBar>
+      
       <Drawer variant="permanent" anchor="left">
         {/* TODO add a real sidebar*/}
         <List>
@@ -29,7 +35,21 @@ const Layout = ({ children }) => {
               <ListItemText primary="lol"></ListItemText>
             </ListItemButton>
           </ListItem>
+          <ListItem key={'second'}>
+            <ListItemButton>
+              <ListItemIcon>
+                <IcecreamOutlined></IcecreamOutlined>
+              </ListItemIcon>
+              <ListItemText primary="lol covered ^"></ListItemText>
+            </ListItemButton>
+          </ListItem>
         </List>
+      </Drawer>
+
+      <Drawer variant="permanent" anchor="right">
+          <Typography variant='h2'>Test My Events</Typography>
+          <Typography variant='h2'>text covered ^</Typography>
+          {/* TODO add a real My Events section*/}
       </Drawer>
       <main>{children}</main>
     </>
