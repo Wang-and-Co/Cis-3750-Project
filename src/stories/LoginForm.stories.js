8f1a5fc3f37a9dd49fcfdb62ebc05013797e7237
Daniel Wang <48870892+DanWang725@users.noreply.github.com>
2024-03-15 16:19:30 -0400

    #27 add storybook

---
+++ src/stories/LoginForm.stories.js
@@ -0,0 +1,41 @@
+import { fn } from '@storybook/test';
+import InputField from '../shared-components/form/InputField/InputField';
+import { Formik } from 'formik';
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+import { object, string } from 'yup';
+import { ThemeProvider } from '@mui/material';
+import { theme } from '../app/themeUtils';
+import { LoginForm } from '../shared-components/form/LoginForm';
+
+let validationSchem = object({
+  name: string().required('Enter a name please'),
+  password: string().required('Enter a password'),
+});
+let initialValues = { name: '', password: '' };
+
+export default {
+  title: 'FORMIK/LoginForm',
+  component: LoginForm,
+  decorators: [
+    (Story) => (
+      <ThemeProvider theme={theme}>
+        <Story />
+      </ThemeProvider>
+    ),
+  ],
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: 'centered',
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs'],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+  args: { onSubmit: fn() },
+};
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Primary = {};
