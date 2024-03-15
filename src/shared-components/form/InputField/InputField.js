7ec4527c2fcd71b4732d11363514d5c3c460be2a
Daniel Wang <48870892+DanWang725@users.noreply.github.com>
2024-03-15 16:18:50 -0400

    #27 adjust styling on input field

--- src/shared-components/form/InputField/InputField.js
+++ src/shared-components/form/InputField/InputField.js
@@ -1,18 +1,26 @@
 import { TextField } from '@mui/material';
 import { useField } from 'formik';
 import PropTypes from 'prop-types';
-const InputField = ({ name, label, required, variant, ...otherProps }) => {
+const InputField = ({
+  name,
+  label,
+  helperText,
+  required,
+  variant,
+  ...otherProps
+}) => {
   const [field, meta] = useField({ name });
   return (
     <TextField
       id={`input-${name}`}
       label={label}
-      error={!!meta.error}
-      helperText={meta.error}
+      error={!!meta.error && meta.touched}
+      helperText={meta.touched && meta.error ? meta.error : helperText}
       required={required}
       {...(variant ? { variant: variant } : {})}
       {...field}
       name={name}
+      sx={{ minHeight: '5rem' }}
       {...otherProps}
     ></TextField>
   );
