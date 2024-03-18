4fc2ef72c0c30ec16699fa5f5dca9a32aa069029
Ethan Wang <wange@uoguelph.ca>
2024-03-18 13:15:25 -0400

    formatted the home page

--- src/app/Layout.js
+++ src/app/Layout.js
@@ -3,6 +3,9 @@ import {
   AppBar,
   CssBaseline,
   Drawer,
+  Box,
+  IconButton,
+  MenuIcon,
   List,
   ListItem,
   ListItemButton,
@@ -12,48 +15,96 @@ import {
   Typography,
 } from '@mui/material';
 
+const drawerWidth = 240;
+const myEventsWidth = 480;
+
+const navBarLeft = (
+  <div>
+    <Toolbar />
+    <List>
+      <ListItem key={'test'} sx={{ marginTop: '2rem' }}>
+        <ListItemButton>
+          <ListItemIcon>
+            <IcecreamOutlined></IcecreamOutlined>
+          </ListItemIcon>
+          <ListItemText primary="lol"></ListItemText>
+        </ListItemButton>
+      </ListItem>
+      <ListItem key={'second'}>
+        <ListItemButton>
+          <ListItemIcon>
+            <IcecreamOutlined></IcecreamOutlined>
+          </ListItemIcon>
+          <ListItemText primary="lol covered ^"></ListItemText>
+        </ListItemButton>
+      </ListItem>
+    </List>
+  </div>
+);
+
+const myEventsBar = (
+  <div style={{ marginTop: '4rem'}}>
+    <Typography variant='h2'>Test My Events</Typography>
+  </div>
+);
+
 const Layout = ({ children }) => {
+
   return (
     <>
       <CssBaseline />
-      <AppBar sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
-        <Toolbar>
-          <IcecreamOutlined />
-          <Typography variant='h4'>Test search bar</Typography> 
-          {/* TODO add a real app bar */}
-        </Toolbar>
-      </AppBar>
-      
-      <Drawer variant="permanent" anchor="left">
-        {/* TODO add a real sidebar*/}
-        <List>
-          <ListItem key={'test'} sx={{ marginTop: '2rem' }}>
-            <ListItemButton>
-              <ListItemIcon>
-                <IcecreamOutlined></IcecreamOutlined>
-              </ListItemIcon>
-              <ListItemText primary="lol"></ListItemText>
-            </ListItemButton>
-          </ListItem>
-          <ListItem key={'second'}>
-            <ListItemButton>
-              <ListItemIcon>
-                <IcecreamOutlined></IcecreamOutlined>
-              </ListItemIcon>
-              <ListItemText primary="lol covered ^"></ListItemText>
-            </ListItemButton>
-          </ListItem>
-        </List>
-      </Drawer>
 
-      <Drawer variant="permanent" anchor="right">
-          <Typography variant='h2'>Test My Events</Typography>
-          <Typography variant='h2'>text covered ^</Typography>
-          {/* TODO add a real My Events section*/}
-      </Drawer>
+      <Box sx={{ display: 'flex' }}>
+        
+        {/*right nav bar*/}
+        <Drawer
+          variant="permanent"
+          anchor="right"
+          sx={{
+            flexShrink: 0,
+            '& .MuiDrawer-paper': { width: myEventsWidth }
+          }}
+          open
+        >
+          {myEventsBar}
+        </Drawer>
+
+        {/*left nav bar*/}
+        <Drawer
+
+          variant="permanent"
+          sx={{
+            width: drawerWidth,
+            flexShrink: 0,
+            '& .MuiDrawer-paper': { width: drawerWidth }
+          }}
+          open
+        >
+          {navBarLeft}
+        </Drawer>
+        
+        <AppBar
+          position='fixed'
+          sx={{
+            width: { sm: `calc(100% - ${drawerWidth}px)` },
+            ml: { sm: `${drawerWidth}px` },
+            zIndex: (theme) => theme.zIndex.drawer + 1
+          }}
+        >
+           {/*top nav bar*/}
+          <Toolbar>
+            <Typography variant='h4'>Test search bar</Typography>
+          </Toolbar>
+        </AppBar>
       
-      <main>{children}</main>
+        {/*body*/}
+        <Box component="main" sx={{ flexGrow: 1, marginRight: `${myEventsWidth}px`}}>
+          {children}
+        </Box>
+      </Box>
     </>
   );
 };
+
+
 export default Layout;
