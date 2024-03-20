12ed78cfae1957a488168c0a0a449d88f57875c7
Daniel Wang <48870892+DanWang725@users.noreply.github.com>
2024-03-20 15:48:59 -0400

    update event api

--- src/app/api/events.js
+++ src/app/api/events.js
@@ -1,10 +1,14 @@
 import { Axios } from '../../shared-components/axios/Axios';
 const retrieveEvents = async (cookies, params) => {
-  const id = cookies?.auth?.id;
-  const options = id
-    ? { params: { ...params, ...(id ? { id: id } : {}) } }
-    : {};
-  const { data } = await Axios.get('/events', options);
-  return data;
+  try {
+    const id = cookies?.auth?.id;
+    const options = id
+      ? { params: { ...params, ...(id ? { id: id } : {}) } }
+      : {};
+    const { data } = await Axios.get('/events', options);
+    return { status: 200, data };
+  } catch (error) {
+    return { status: 0, ...error };
+  }
 };
 export { retrieveEvents };
