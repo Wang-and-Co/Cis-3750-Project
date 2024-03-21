40bb9becf8b2ddb9fad0dc464ab1c56a31d8a07b
Daniel Wang <48870892+DanWang725@users.noreply.github.com>
2024-03-21 14:29:23 -0400

    #55 fix axios error

--- src/shared-components/axios/Axios.js
+++ src/shared-components/axios/Axios.js
@@ -3,6 +3,6 @@
 import axios from 'axios';
 
 export const Axios = axios.create({
-  baseURL: process?.env?.REACT_APP_FLASK_URL ?? 'http://localhost:3000', // use env variable to determine url to retrieve from
+  baseURL: process.env?.REACT_APP_FLASK_URL ?? 'http://localhost:3000', // use env variable to determine url to retrieve from
   timeout: 1000,
 });
