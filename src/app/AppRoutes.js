26f81db80bb5e32a9e42839621eafbfed812f820
Daniel Wang <48870892+DanWang725@users.noreply.github.com>
2024-03-21 11:24:06 -0400

    #55 add terms and conditions to routes

--- src/app/AppRoutes.js
+++ src/app/AppRoutes.js
@@ -1,10 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import SamplePage from '../screens/SamplePage';
 import ErrorPage from '../screens/ErrorPage';
+import React from 'react';
 
+const Terms = React.lazy(() => import('./../screens/TermsAndConditions'));
 const AppRoutes = () => (
   <Routes>
     <Route path="/" exact element={<SamplePage />} />
+    <Route path="/legal/terms-of-service" element={<Terms />} />
     <Route path="*" element={<ErrorPage />} />
   </Routes>
 );
