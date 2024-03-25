89d45a37564b8195169acfb056d599702477e1e6
Ethan Wang <wange@uoguelph.ca>
2024-03-25 14:50:27 -0400

    created footer

---
+++ src/shared-components/Navigation/Footer.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography, Link } from '@mui/material';
+
+const Footer = () => {
+  const NAVBAR_LEFT_WIDTH_PERCENT = 15;
+
+  return (
+    <AppBar
+      position="static"
+      sx={{
+        top: 'auto',
+        bottom: 0,
+        backgroundColor: 'primary',
+        color: 'white',
+        width: `${100 - NAVBAR_LEFT_WIDTH_PERCENT}%`,
+        marginLeft: `${NAVBAR_LEFT_WIDTH_PERCENT}%`,
+      }}
+    >
+      <Toolbar sx={{ justifyContent: 'space-between' }}>
+        <Typography variant="body1" component="div">
+          &copy; {' '}
+          <Link href="https://github.com/Wang-and-Co" color="inherit" underline="none">
+              Wang & Co.
+          </Link>
+        </Typography>
+        <div>
+          <Link href="http://localhost:3000/legal/terms-of-service" color="inherit" underline="none" style={{ marginRight: 10 }}>
+            Terms of Service & Privacy Policy
+          </Link>
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Footer;
