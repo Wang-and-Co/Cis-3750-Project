2f573f0a8a28e5b160856feaed51c1632cc1089d
Ethan Wang <wange@uoguelph.ca>
2024-03-25 19:59:13 -0400

    prettied up hosting page

--- src/screens/HostingPage/HostingPage.js
+++ src/screens/HostingPage/HostingPage.js
@@ -1,14 +1,41 @@
-import { Box, Button, Typography } from '@mui/material';
+import { Box, Button, Typography, CardMedia} from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import eventImage from '../../assets/guelph.jpg'
+import cityHall from '../../assets/cityhall.jpg'
+import community from '../../assets/community.jpg'
 
 const HostingPage = () => {
   const navigate = useNavigate();
   return (
-    <Box>
-      <Typography variant="h1">Welcome to hosting page!</Typography>
-      <Button onClick={() => navigate('./create', { relative: 'path' })}>
-        Create an event
-      </Button>
+    <Box sx={{ textAlign: 'center', position: 'relative' }}>
+      <CardMedia
+        component="img"
+        height="610"
+        image={eventImage}
+        style={{ width: '100%', marginBottom: '2rem' }}
+        alt="Event Image"
+      />
+      <Box>
+        <Typography variant="h1">Welcome to the Hosting Page!</Typography>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => navigate('./create', { relative: 'path' })}
+          sx={{margin: '2.2rem'}}
+        >
+          Create an Event
+        </Button>
+        <Typography variant="h4">
+          A platform created for the local community of Guelph.
+        </Typography>
+        <CardMedia
+          component="img"
+          height="610"
+          image={community}
+          style={{ width: '100%', marginTop: '2rem' }}
+          alt="Event Image"
+        />
+      </Box>
     </Box>
   );
 };
