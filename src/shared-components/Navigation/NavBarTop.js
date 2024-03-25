b65330cebc3ffade1e5e81f0d69adab15a984b74
rblotsky <roman.blotsky@gmail.com>
2024-03-24 21:22:45 -0400

    Working on navbartop

--- src/shared-components/Navigation/NavBarTop.js
+++ src/shared-components/Navigation/NavBarTop.js
@@ -1,4 +1,4 @@
-import { Toolbar, Typography, AppBar } from '@mui/material';
+import { Toolbar, Typography, AppBar, Box, Button } from '@mui/material';
 
 const NavBarTop = () => {
   return (
@@ -13,6 +13,16 @@ const NavBarTop = () => {
       }}
     >
       <Toolbar>
+        <Button
+          onClick={() => {
+            console.log('Pressed!');
+          }}
+        >
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/f/f7/No_Image_%282879926%29_-_The_Noun_Project.svg"
+            alt="ComBo Logo"
+          />
+        </Button>
         <Typography variant="h6" noWrap component="div">
           Search bar goes here
         </Typography>
