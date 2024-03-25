fa373d2e0660f4663c7491c0abb00d2227614275
rblotsky <roman.blotsky@gmail.com>
2024-03-24 22:46:04 -0400

    Implemented most of navbar - now just need to style it

--- src/shared-components/Navigation/NavBarTop.js
+++ src/shared-components/Navigation/NavBarTop.js
@@ -1,6 +1,18 @@
-import { Toolbar, Typography, AppBar, Box, Button } from '@mui/material';
+import {
+  Toolbar,
+  Typography,
+  AppBar,
+  Box,
+  Button,
+  TextField,
+  InputAdornment,
+} from '@mui/material';
+import { InputField } from '../form/InputField';
+import { useCookies } from 'react-cookie';
+import { Search } from '@mui/icons-material';
 
 const NavBarTop = () => {
+  const [cookies, setCookies, removeCookie] = useCookies(['auth']);
   return (
     <AppBar
       position="fixed"
@@ -10,22 +22,74 @@ const NavBarTop = () => {
         backgroundColor: 'white',
         borderBottom: 1,
         borderColor: 'lightgray',
+        height: '4rem',
       }}
     >
-      <Toolbar>
+      <Toolbar
+        disableGutters
+        sx={{
+          height: '100%',
+          paddingRight: 5,
+        }}
+      >
         <Button
           onClick={() => {
             console.log('Pressed!');
           }}
+          sx={{ padding: 1, margin: 0, height: '100%', width: '15%' }}
         >
           <img
-            src="https://upload.wikimedia.org/wikipedia/commons/f/f7/No_Image_%282879926%29_-_The_Noun_Project.svg"
+            src={'ComboLogo.png'}
             alt="ComBo Logo"
+            style={{
+              height: '100%',
+              width: '100%',
+              objectFit: 'contain',
+            }}
           />
         </Button>
-        <Typography variant="h6" noWrap component="div">
-          Search bar goes here
-        </Typography>
+        <TextField
+          id="search"
+          placeholder="Search for an event..."
+          variant="outlined"
+          disabled={cookies.auth == null}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <Search />
+              </InputAdornment>
+            ),
+          }}
+          sx={{
+            flexGrow: 1,
+          }}
+        />
+        {cookies.auth ? (
+          <Button
+            variant="contained"
+            title="Log Out"
+            sx={{ marginLeft: 1, height: '80%' }}
+          >
+            Log Out
+          </Button>
+        ) : (
+          <>
+            <Button
+              variant="contained"
+              title="Log In"
+              sx={{ marginLeft: 1, height: '80%' }}
+            >
+              Log In
+            </Button>
+            <Button
+              variant="contained"
+              title="Sign Up"
+              sx={{ marginLeft: 1, height: '80%' }}
+            >
+              Sign Up
+            </Button>
+          </>
+        )}
       </Toolbar>
     </AppBar>
   );
