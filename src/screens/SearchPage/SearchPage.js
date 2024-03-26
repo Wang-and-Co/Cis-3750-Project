8dae62a2a8ba824e0b7f534c2b70fcac5ebb4876
rblotsky <roman.blotsky@gmail.com>
2024-03-26 19:34:45 -0400

    Prettied up search page and also improved API calls in it

--- src/screens/SearchPage/SearchPage.js
+++ src/screens/SearchPage/SearchPage.js
@@ -1,39 +1,110 @@
-import { Button, LinearProgress, Typography } from '@mui/material';
+import {
+  Box,
+  Button,
+  Container,
+  LinearProgress,
+  Typography,
+} from '@mui/material';
 import { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
-import { searchEvents } from '../../app/api/events';
+import { retrieveEvents, searchEvents } from '../../app/api/events';
 import useAsyncResponse from '../../shared-components/axios/useAsyncResponse';
 import toast from 'react-hot-toast';
 import EventsGrid from '../../shared-components/Navigation/EventsGrid';
+import useAuth from '../../shared-components/hooks/useAuth';
+import * as sampleEvents from '../../stories/sampleEvents.js';
+import SidebarRight from '../../shared-components/Navigation/SidebarRight';
+import MyEventsStack from '../../shared-components/Navigation/MyEventsStack';
+import EventDescription from '../../shared-components/event-display/EventDescription.js';
+
+const SIDEBAR_RIGHT_WIDTH_PERCENT = 25;
 
 const SearchPage = () => {
+  const { isLoggedIn, setAuthInfo, handleLogout } = useAuth();
   const [searchParams] = useSearchParams();
   const [searchResults, setSearchResults] = useState([]);
-  const { isLoading, callAsyncFunction } = useAsyncResponse(
-    searchEvents,
-    setSearchResults,
-    () => toast('something went wrong, please try again'),
+  const [registeredEvents, setRegisteredEvents] = useState([]);
+  const [currentViewedEvent, setCurrentViewedEvent] = useState(null);
+
+  // Fetching data from server
+  const { isLoading: isSearchLoading, callAsyncFunction: callSearchAsync } =
+    useAsyncResponse(
+      searchEvents,
+      (eventData) => {
+        setSearchResults(eventData.data);
+      },
+      () => toast('Something went wrong (search results), please try again.'),
+    );
+
+  const {
+    isLoading: isRegisteredLoading,
+    callAsyncFunction: callRegisteredAsync,
+  } = useAsyncResponse(
+    retrieveEvents,
+    (eventData) => {
+      setRegisteredEvents(eventData.data);
+    },
+    () => toast('Something went wrong (registered events), please try again.'),
   );
+
+  // Effects
   useEffect(() => {
-    callAsyncFunction({ name: `${searchParams.get('name')}` });
+    callSearchAsync({ name: `${searchParams.get('name')}` });
   }, [searchParams]);
+
+  useEffect(() => {
+    callRegisteredAsync();
+  }, [currentViewedEvent, searchParams]);
+
+  const sidebarWidthToUse = isLoggedIn ? SIDEBAR_RIGHT_WIDTH_PERCENT + 3 : 0;
+
   return (
-    <div>
-      {isLoading && <LinearProgress />}
-      <Typography variant="h3" align="center" marginTop={'2rem'}>
-        {`Results for events named: '${searchParams.get('name')}'`}
-      </Typography>
-      {/* <EventsGrid
-        events={searchResults}
-        eventDetailsOpenFunc={(event) => {
-          if (isLoggedIn) {
-            setCurrentViewedEvent(event);
-          } else {
-            toast('You must be logged in to view events!');
-          }
+    <Box width={`${100 - sidebarWidthToUse}%`}>
+      {isSearchLoading && <LinearProgress />}
+
+      <Container
+        sx={{
+          paddingRight: sidebarWidthToUse > 0 ? 5 : 0,
         }}
-      /> */}
-    </div>
+      >
+        <Typography variant="h6" align="left" marginTop={'2rem'}>
+          {`We found ${searchResults.length > 0 ? searchResults.length : 0} results for events named '${searchParams.get('name')}'.`}
+        </Typography>
+        {searchResults.length > 0 ? (
+          <EventsGrid
+            events={searchResults.length > 0 ? searchResults : []}
+            eventDetailsOpenFunc={(event) => {
+              if (isLoggedIn) {
+                setCurrentViewedEvent(event);
+              } else {
+                toast('You must be logged in to view events!');
+              }
+            }}
+          />
+        ) : (
+          <></>
+        )}
+      </Container>
+      {isLoggedIn ? (
+        <SidebarRight widthPercent={SIDEBAR_RIGHT_WIDTH_PERCENT}>
+          {currentViewedEvent == null ? (
+            <MyEventsStack
+              events={registeredEvents}
+              eventDetailsOpenFunc={setCurrentViewedEvent}
+            />
+          ) : (
+            <EventDescription
+              closeFunc={() => {
+                setCurrentViewedEvent(null);
+              }}
+              event={currentViewedEvent}
+            />
+          )}
+        </SidebarRight>
+      ) : (
+        <></>
+      )}
+    </Box>
   );
 };
 export default SearchPage;
