1bbbf0d7e3a86bbe2f309230485719da84a94db6
Daniel Wang <48870892+DanWang725@users.noreply.github.com>
2024-03-26 13:11:44 -0400

    #67 add error handling, improve code

--- src/screens/CreateEventPage/CreateEventPage.js
+++ src/screens/CreateEventPage/CreateEventPage.js
@@ -1,28 +1,26 @@
-import { Container, Typography } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import CreateEventForm from '../../shared-components/form/CreateEventForm/CreateEventForm';
 import toast from 'react-hot-toast';
 import sampleImage from '../../assets/sampleImage.png';
-import {
-  getFormattedFormPayload,
-  getLocationFromString,
-  getLocationString,
-} from '../../shared-components/form/CreateEventForm/utils';
+import { getFormattedFormPayload } from '../../shared-components/form/CreateEventForm/utils';
 import useAsyncResponse from '../../shared-components/axios/useAsyncResponse';
 import { postNewEvent } from '../../app/api/events';
 
 const CreateEventPage = () => {
   const navigate = useNavigate();
-  const { isLoading, callAsyncFunctionPromise } =
-    useAsyncResponse(postNewEvent);
+  const { callAsyncFunctionPromise } = useAsyncResponse(postNewEvent);
 
   const handleSubmit = async (values, actions) => {
-    console.log(values);
     getFormattedFormPayload(values);
     const payload = getFormattedFormPayload(values);
-    console.log('Payload!!!', payload);
-    const { data, status } = await callAsyncFunctionPromise(payload);
-    console.log(data, status);
+    // console.log('Payload!!!', payload);
+    const { data, status } = await callAsyncFunctionPromise(payload); //TODO: navigate to the created event
+    // console.log(data, status);
+    if (status !== 200) {
+      actions.setSubmitting(false);
+      toast('Something went wrong. Please try again.');
+      return;
+    }
 
     actions.setSubmitting(false);
     navigate('/hosting');
