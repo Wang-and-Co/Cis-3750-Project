517beab488d131926fae66fc1f483d4066403872
Daniel Wang <48870892+DanWang725@users.noreply.github.com>
2024-03-26 19:58:27 -0400

    #40 add parsing function

--- src/app/api/events.js
+++ src/app/api/events.js
@@ -45,21 +45,28 @@ const addBooking = async (payload) => {
 };
 
 const deleteBooking = async (params) => {
-  try{
-    const { data } = await Axios.delete('/eventBooking', { params })
-    return { status: 200, ...data};
+  try {
+    const { data } = await Axios.delete('/eventBooking', { params });
+    return { status: 200, ...data };
   } catch (error) {
-    return {status: 400, ...error};
+    return { status: 400, ...error };
   }
-}
+};
 
 const deleteEvent = async (params) => {
   try {
-    const { data } = await Axios.delete('/events', { params })
-    return { status: 200, ...data};
+    const { data } = await Axios.delete('/events', { params });
+    return { status: 200, ...data };
   } catch (error) {
-    return { status: 400, ...error};
+    return { status: 400, ...error };
   }
-}
+};
 
-export { retrieveEvents , retrieveBookings, addBooking, deleteBooking, deleteEvent };
+export {
+  retrieveEvents,
+  retrieveBookings,
+  addBooking,
+  deleteBooking,
+  deleteEvent,
+  postNewEvent,
+};
