3cb2ff227b2391b7ed81614700d2db61d03ddeca
Ethan Wang <wange@uoguelph.ca>
2024-03-26 16:03:53 -0400

    reformated hosting page

--- src/screens/HostingPage/HostingPage.js
+++ src/screens/HostingPage/HostingPage.js
@@ -6,22 +6,32 @@ import community from '../../assets/community.jpg'
 const HostingPage = () => {
   const navigate = useNavigate();
   return (
-    <Box sx={{ textAlign: 'center', position: 'relative' }}>
+    <Box sx={{ textAlign: 'center', position: 'relative'}}>
       <CardMedia
         component="img"
         image={eventImage}
         style={{ width: '100%', height: '66vh', marginBottom: '2rem' }}
         alt="Event Image"
       />
-      <Box>
-        <Typography variant="h1">Welcome to the Hosting Page!</Typography>
+      <Box sx={{ 
+        position: 'absolute', 
+        top: '40%', 
+        left: '50%', 
+        transform: 'translate(-50%, -50%)', 
+        backgroundColor: '#EBEBEB', 
+        
+        padding: 1}}>
+        <Typography variant="h1" sx={{}}>Welcome to the Hosting Page!</Typography>
+      </Box>
+      <Box sx={{ marginTop: '6rem' }}>
         <Button
           variant="contained"
           color="primary"
+          size="large"
           onClick={() => navigate('./create', { relative: 'path' })}
-          sx={{margin: '2.2rem'}}
+          sx={{margin: '1.5rem'}}
         >
-          Create an Event
+          Create a local event today
         </Button>
         <Typography variant="h4">
           A platform created for the local community of Guelph.
