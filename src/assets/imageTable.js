521265acdbec316ef69358182a0b34139daf9ff6
Daniel Wang <48870892+DanWang725@users.noreply.github.com>
2024-03-26 23:31:48 -0400

    add images

---
+++ src/assets/imageTable.js
@@ -0,0 +1,28 @@
+import image from './amongUs.png';
+import image2 from './amongUsFungle.png';
+import image3 from './bikingGuelph.jpg';
+import image4 from './bikingNiagara.jpeg';
+import image5 from './books.jpg';
+import image6 from './dodgeball.jpg';
+import image7 from './guelph.jpg';
+import image8 from './jackbox.png';
+import image9 from './pythonLanguage.png';
+import image10 from './pythonReal.jpg';
+import image11 from './scratch.png';
+import image12 from './yogaGuelph.jpg';
+import image13 from './yogaGuelph2.jpg';
+export const imageMap = {
+  'amongIs.png': image,
+  'amongUsFungle.png': image2,
+  'bikingGuelph.jpg': image3,
+  'bikingNiagara.jpeg': image4,
+  'books.jpg': image5,
+  'dodgeball.jpg': image6,
+  'guelph.jpg': image7,
+  'jackbox.png': image8,
+  'pythonLanguage.png': image9,
+  'pythonReal.jpg': image10,
+  'scratch.png': image11,
+  'yogaGuelph.jpg': image12,
+  'yogaGuelph2.jpg': image13,
+};
